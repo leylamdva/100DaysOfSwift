cffc23973e2dae8b48b44fe06155c730c2951790
leylamdva <l.mammadova@hotmail.com>
2022-07-25 22:44:50 +0300

    GuessTheFlag added

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Leyla Mammadova on 7/25/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
