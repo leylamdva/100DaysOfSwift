a9ecc57475ba23edcc231c8ce50b457d3bbc4a40
leylamdva <l.mammadova@hotmail.com>
2022-08-13 13:49:15 +0400

    Modified total amount text view

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -63,6 +63,7 @@ struct ContentView: View {
                 }
                 Section{
                     Text(totalCheck, format: specifiedCurrency)
+                        .foregroundColor(tipPercentage == 0 ? .red : .black)
                 }header: {
                     Text("Total Amount")
                 }
